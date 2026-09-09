9d22d5247d27ac9630a85f997ebd03ada1aff6f1
fahim-5 <fahimbafu@gmail.com>
2026-09-09 09:40:17 +0600

    partnerticker fixed

--- src/config/partners.js
+++ src/config/partners.js
@@ -1,42 +1,38 @@
+import dhakaBankLogo from "@/assets/PartnerTicker/799723993_1583434440186347_1561948345481984224_n.jpg";
+import bKashLogo from "@/assets/PartnerTicker/799501820_2318504218951755_6252182063360764220_n.jpg";
+import bankAbcLogo from "@/assets/PartnerTicker/796457750_1897762764541613_3103392278845257516_n.png";
+import rupaliBankLogo from "@/assets/PartnerTicker/795483939_1052172440747764_5135838064843517309_n.png";
+import sonaliBankLogo from "@/assets/PartnerTicker/793593238_4602701876677449_988427859291171503_n.jpg";
+import islamiBankLogo from "@/assets/PartnerTicker/760603694_1737867087435390_1431949280439256554_n.jpg";
+import pubaliBankLogo from "@/assets/PartnerTicker/746439772_1821821109232521_4948989871735686925_n.jpg";
+
 export const PARTNERS = [
   {
-    name: "Google",
-    monogram: "G",
+    name: "Dhaka Bank Limited",
+    image: dhakaBankLogo,
   },
   {
-    name: "Microsoft",
-    monogram: "M",
+    name: "bKash",
+    image: bKashLogo,
   },
   {
-    name: "Apple",
-    monogram: "A",
+    name: "Bank ABC",
+    image: bankAbcLogo,
   },
   {
-    name: "Amazon",
-    monogram: "a",
+    name: "Rupali Bank PLC",
+    image: rupaliBankLogo,
   },
   {
-    name: "LinkedIn",
-    monogram: "in",
+    name: "Sonali Bank PLC",
+    image: sonaliBankLogo,
   },
   {
-    name: "Facebook",
-    monogram: "f",
+    name: "Islami Bank Bangladesh PLC",
+    image: islamiBankLogo,
   },
   {
-    name: "YouTube",
-    monogram: "YT",
-  },
-  {
-    name: "GitHub",
-    monogram: "GH",
-  },
-  {
-    name: "Slack",
-    monogram: "S",
-  },
-  {
-    name: "Notion",
-    monogram: "N",
+    name: "Pubali Bank PLC",
+    image: pubaliBankLogo,
   },
 ];
